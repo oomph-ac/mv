b1a0a329653818914fc044a2d6ace0ad7232c814
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-05-04 17:52:35 -0400

    add back item mappings

--- multiversion/mv662/mappings.go
+++ multiversion/mv662/mappings.go
@@ -3,17 +3,18 @@ package mv662
 import (
 	_ "embed"
 
-	"github.com/oomph-ac/mv/multiversion/latest"
 	"github.com/oomph-ac/mv/multiversion/mappings"
 )
 
 var (
 	//go:embed mappings/block_states.nbt
 	blockStates []byte
+	//go:embed mappings/item_runtime_ids.nbt
+	itemRuntimeIDs []byte
 
 	Mapping mappings.MVMapping
 )
 
 func init() {
-	Mapping = mappings.Mapping(blockStates, latest.ItemRuntimeIDData, false)
+	Mapping = mappings.Mapping(blockStates, itemRuntimeIDs, false)
 }
